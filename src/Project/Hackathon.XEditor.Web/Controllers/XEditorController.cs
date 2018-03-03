85ab438880f77018cab2d8c6d7ccb2b0c81ea5ec
Vlad Ziziko <ziziko.vlad@gmail.com>
2018-03-03 15:33:46 +0300

    add editor view

--- src/Project/Hackathon.XEditor.Web/Controllers/XEditorController.cs
+++ src/Project/Hackathon.XEditor.Web/Controllers/XEditorController.cs
@@ -1,13 +1,34 @@
-﻿namespace Hackathon.XEditor.Web.Controllers
+﻿using System.Threading.Tasks;
+using System.Web;
+using Hackathon.XEditor.Api.Dto;
+using Hackathon.XEditor.Api.Services;
+
+namespace Hackathon.XEditor.Web.Controllers
 {
     using System;
     using System.Web.Mvc;
 
     public class XEditorController : Controller
     {
-        public ActionResult EditorForm(Guid id)
+        private XconnectService _xconnectService;
+
+        private static string ContactIdParam = "cid";
+
+        public XEditorController() : this(new XconnectService())
         {
-            return Content("Empty rendering");
+        }
+
+        public XEditorController(XconnectService xconnectService)
+        {
+            _xconnectService = xconnectService;
+        }
+
+        public ActionResult EditorForm()
+        {
+            string cid = HttpUtility.ParseQueryString(Request.UrlReferrer.Query)[ContactIdParam];
+            ContactDto model = Task.Run(() => _xconnectService.GetContact(new Guid(cid))).Result;
+
+            return View("/Views/XEditor/UserDataEditorForm.cshtml", model);
         }
     }
 }
