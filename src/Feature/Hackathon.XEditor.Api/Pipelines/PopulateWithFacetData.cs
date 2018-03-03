6c9c7ee7dbf0c163883802c8e2887b7ab1b1ccd0
x3mxray <x3m.xray@gmail.com>
2018-03-03 21:25:36 +0300

    Styling facet

--- src/Feature/Hackathon.XEditor.Api/Pipelines/PopulateWithFacetData.cs
+++ src/Feature/Hackathon.XEditor.Api/Pipelines/PopulateWithFacetData.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Reflection;
-using Hackathon.XEditor.Api.Dto;
 using Hackathon.XEditor.Api.Services;
-using Hackathon.XEditor.Model.Contact.Facets;
 using Sitecore.Cintel.Reporting;
 using Sitecore.Cintel.Reporting.Processors;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Practices.EnterpriseLibrary.Common.Utility;
 
 namespace Hackathon.XEditor.Api.Pipelines
 {
     public  class PopulateWithFacetData: ReportProcessorBase
     {
+        private static readonly Type BaseType = typeof(Sitecore.XConnect.Facet);
+        readonly List<string> _skippedProperties = BaseType.GetProperties().Select(x => x.Name).ToList();
+        private const int maxDepth = 3;
+        private bool IsDefaultProperty(string propertyName)
+        {
+                return _skippedProperties.Contains(propertyName);
+        }
+
         public override void Process(ReportProcessorArgs args)
         {
             var xConnectService = new XconnectService();
@@ -23,43 +27,87 @@ namespace Hackathon.XEditor.Api.Pipelines
             args.ResultSet.Data.Dataset[args.ReportParameters.ViewName] = table;
         }
 
-        public static DataTable CreateDataTable(IEnumerable<dynamic> list)
+        public DataTable CreateDataTable(IEnumerable<dynamic> list)
         {
-            Type baseType = typeof(Sitecore.XConnect.Facet);
-            var skippedProperties = baseType.GetProperties().Select(x => x.Name).ToList();
-
             DataTable dataTable = new DataTable();
 
             dataTable.Columns.Add(new DataColumn("FacetName", "".GetType()));
             dataTable.Columns.Add(new DataColumn("FieldName", "".GetType()));
             dataTable.Columns.Add(new DataColumn("FieldValue", "".GetType()));
+            dataTable.Columns.Add(new DataColumn("DepthLevel", "".GetType()));
 
             foreach (var entity in list)
             {
-                Type type = entity.GetType();
-                var properties = type.GetProperties();
-                foreach (var p in properties)
-                {
-                    object[] values = new object[3];
+                AddChildren(ref dataTable, entity,0);
+                //Type type = entity.GetType();
+                //var properties = type.GetProperties();
+                //foreach (var p in properties)
+                //{
+                //    object[] values = new object[3];
 
-                    values[0] = type.Name;
-                    values[1] = p.Name;
-                    var value = p.GetValue(entity) ?? "";
-                    values[2] = value;
-                    Type valueType = value.GetType();
-                    if (valueType.FullName != null)
-                    {
-                        if (!valueType.FullName.StartsWith("System.Collections"))
-                            if (!skippedProperties.Contains(p.Name))
-                                dataTable.Rows.Add(values);
-                    }
-                   
-
-                }
+                //    values[0] = type.Name;
+                //    values[1] = p.Name;
+                //    var value = p.GetValue(entity) ?? "";
+                //    values[2] = value;
+                //    Type valueType = value.GetType();
+                //    if (valueType.FullName != null)
+                //    {
+                //        if (!valueType.FullName.StartsWith("System.Collections"))
+                //            if (!IsDefaultProperty(p.Name))
+                //            {
+                //                if (valueType.IsValueType)
+                //                {
+                //                    dataTable.Rows.Add(values);
+                //                }
+                //                else
+                //                {
+                //                    values[2] = "$object$";
+                //                    dataTable.Rows.Add(values);
+                //                    AddChildren(ref dataTable, value);
+                //                }
+                //            }
+                //    }
+                //}
                
             }
 
             return dataTable;
         }
+
+        private void AddChildren(ref DataTable dataTable, dynamic obj, int depth)
+        {
+            if (depth > maxDepth) return; 
+            Type type = obj.GetType();
+            var properties = type.GetProperties();
+            foreach (var p in properties)
+            {
+                object[] values = new object[4];
+
+                values[0] = type.Name;
+                values[1] = p.Name;
+                var value = p.GetValue(obj) ?? "";
+                values[2] = value;
+                values[3] = depth;
+                Type valueType = value.GetType();
+                if (valueType.FullName != null)
+                {
+                    if (!valueType.FullName.StartsWith("System.Collections"))
+                        if (!IsDefaultProperty(p.Name))
+                        {
+                            if (valueType.Namespace.StartsWith("System"))
+                            {
+                                dataTable.Rows.Add(values);
+                            }
+                            else
+                            {
+                                values[2] = "$object$";
+                                dataTable.Rows.Add(values);
+                                AddChildren(ref dataTable, value, depth+1);
+                            }
+                        }
+                }
+            }
+        }
+
     }
 }
