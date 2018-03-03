1a174935e4b34da15a4dc67b68d475cb8671fd49
danl <d.raschupkin@brimit.com>
2018-03-03 11:22:21 +0300

    add a controller, add test contact

---
+++ src/Feature/Hackathon.XEditor.Api/Controllers/ContactApiController.cs
@@ -0,0 +1,31 @@
+﻿namespace Hackathon.XEditor.Api.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    using Services;
+
+    public class ContactApiController : Controller
+    {
+        private XconnectService _xconnectService;
+
+        private const string TestSource = "test";
+
+        public ContactApiController() : this(new XconnectService())
+        {
+        }
+
+        public ContactApiController(XconnectService xconnectService)
+        {
+            _xconnectService = xconnectService;
+        }
+
+        public async Task<ActionResult> TestAdd()
+        {
+            bool result = await _xconnectService.CreateContact(TestSource, Guid.NewGuid().ToString("D"),"First", "Last", "Mr", "", "test@sitecorehackathon.com");
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
